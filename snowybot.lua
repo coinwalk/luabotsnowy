8bfdfa30bd5f5fff4ba6579da764b61fd837b0b0
coinwalk <40335044+coinwalk@users.noreply.github.com>
2021-08-25 15:32:08 +1000

    Add files via upload

--- snowybot.lua
+++ snowybot.lua
@@ -1,11 +1,11 @@
-y       = balance/64
+y       = balance/512
 nextbet = y
 older   = balance
 old     = balance
 bolder  = balance
 lol     = 1000000
 x       = lol
-target  = 1
+target  = 10000
 chance  = 49.95
 Low     = 0
 High    = 499499
@@ -13,14 +13,11 @@ bethigh = false
 bill    = lol
 bi      = lol
 vim     = false
-bal     = balance
 
 function dobet()
 if win then
-    bal  = bal+previousbet
     bill = bill-1
 else
-    bal  = bal-previousbet
     bill = bill+1
 end  
  if (bill<=x-7) then
@@ -33,22 +30,22 @@ end
      x       = bill
      bill    = x  
  end 
- if (balance<=((older/64)*24)) then
-     nextbet = balance/64
+ if (balance<=((older/512)*192)) then
+     nextbet = balance/512
      x       = lol
      bill    = lol
      older   = balance
      vim     = true
  end
  if ((balance>=old) and (previousbet>=(y*2))) or ((balance>=bolder) and (vim==true)) then
-    nextbet = balance/64
+    nextbet = balance/512
     bill    = lol
     x       = lol
     old     = balance
     older   = balance
     vim     = false
 end
-if bal>target or bal<nextbet then
+if balance>target or balance<nextbet then
         stop()
     end
 end
